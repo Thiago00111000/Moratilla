e4d031a928f2104fd4fcca571cfb8ab49ea348c3
Thiago00111000 <127748415+Thiago00111000@users.noreply.github.com>
2024-05-03 17:59:52 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -13,3 +13,28 @@ inputElement.addEventListener("input", (e) => {
       }
     })
   })
+
+  const humidityElement = document.querySelector("#title span")
+
+   // Lista de URLs de imagens de fundo aleatórias
+const txtAleatorio = [
+  "Batata",
+  "Pão",
+  "Ovo",
+  "Café",
+  ];
+
+// Função para obter um número aleatório entre 0 e o comprimento da lista de imagens
+function getRandomIndex(max) {
+  return Math.floor(Math.random() * max);
+}
+
+// Função para definir uma imagem de fundo aleatória
+function definirImagemAleatoria() {
+  const randomIndex = getRandomIndex(txtAleatorio.length);
+  const randomImageUrl = txtAleatorio[randomIndex];
+  humidityElement.innerText = `${randomImageUrl}`;
+}
+
+// Chamando a função para definir a imagem de fundo aleatória quando a página é carregada
+window.onload = definirImagemAleatoria;
